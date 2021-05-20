18fc47d65fcef5309c094fc41a1e5f39b4e5daf1
pierre9993 <74414278+pierre9993@users.noreply.github.com>
2021-05-20 13:02:23 +0200

    Php Mailer now Working

--- controller/contactController.php
+++ controller/contactController.php
@@ -1,22 +1,18 @@
 <?php
 include_once("controller/verificationController.php");
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require '.env.local';
 
 require 'asset/PHPMailer/src/Exception.php';
 require 'asset/PHPMailer/src/PHPMailer.php';
 require 'asset/PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 class contactController
 {
     //propriétés
-    public $nom;
-    public $prenom;
-    public $email;
-    public $tel;
-    public $message;
-    public $motif;
-    public $select;
     public $verif;
 
     public function __construct()
@@ -28,7 +24,6 @@ class contactController
 
     public function showContact()
     {
-
         //on verifie que l'utilisateur à envoyé un formulaire
         if (isset($_POST['submit'])) {
 
@@ -45,36 +40,42 @@ class contactController
             }
             
             if ($nom && $prenom && $email && $message && $motif) {
-                //IL FAUT ABSOLUMENT FAIRE UNE FONCTION MAIL SINON ON EST DANS LA sauce
-               
-                $mail = new PHPMailer(True);
-                $mail->IsSMTP();
-                $mail->Host = 'localhost';               //Adresse IP ou DNS du serveur SMTP
-                $mail->Port = 25;                          //Port TCP du serveur SMTP
-                $mail->SMTPAuth = 1;                        //Utiliser l'identification
+                //fonction mail
                 
+                $mail = new PHPMailer();
+                $mail->IsSMTP();
+                $mail->Host = 'smtp.gmail.com';               //Adresse IP ou DNS du serveur SMTP
+                $mail->Port = 465;                          //Port TCP du serveur SMTP
+                $mail->SMTPAuth = 1;                        //Utiliser l'identification
+
+
                 if($mail->SMTPAuth){
-                    $mail->SMTPSecure = 'ssl';               //Protocole de sécurisation des échanges avec le SMTP
-                    $mail->Username   =  'm.pierre9993@gmail.com';   //Adresse email à utiliser
-                    $mail->Password   =  'oops';         //Mot de passe de l'adresse email à utiliser
+                    $mail->SMTPSecure = 'ssl';   //Protocole de sécurisation des échanges avec le SMTP
+                    $mail->Username   =  Env::username_mail ;   //Adresse email à utiliser
+                    $mail->Password   =  Env::mdp_mail;         //Mot de passe de l'adresse email à utiliser
                 }
-                $mail->CharSet = 'UTF-8'; //Format d'encodage à utiliser pour les caractères
-                $mail->smtpConnect();
 
-                $mail->From       =  'm.pierre9993@gmail.com';                //L'email à afficher pour l'envoi
-                $mail->FromName   = 'Contact de billy';     
+               $mail->CharSet = 'UTF-8'; //Format d'encodage à utiliser pour les caractères
 
-                $mail->Subject    =  $motif;                      //Le sujet du mail
+                $mail->From       =  Env::username_mail ;                //L'email à afficher pour l'envoi
+                $mail->FromName   = 'Message de '.@$_POST["nom"] . ' '.@$_POST["prenom"];     
+
+                $mail->Subject    ='['. @$_POST["motif"].'] Message de '.@$_POST["nom"] . ' '.@$_POST["prenom"];                      //Le sujet du mail
                 $mail->WordWrap   = 50; 			                   //Nombre de caracteres pour le retour a la ligne automatique
                 $mail->AltBody = $message; 	       //Texte brut
-                $mail->IsHTML(false);             
-                $mail->SMTPDebug = 1;                     //Préciser qu'il faut utiliser le texte brut
-                echo'aaaaaaaa';
-                $mail->AddAddress('morlelucas@gmail.com','John Doe');
+                $mail->IsHTML(false);               //Préciser qu'il faut utiliser le texte brut
+      
+                $mail->AddAddress(Env::username_mail ,'John Doe');
+
+                $mail->Body = '<u>Expéditeur</u> : '. @$_POST["nom"] .' ' . @$_POST["prenom"] .' | Contact : '. @$_POST["email"] .' | '. @$_POST["tel"].' <br/><br/>
+                <u> Statut </u>: '. @$_POST["qui"] .' <br/><br/> 
+                <u>Motif de Contact </u>: '.  @$_POST["motif"] .' <br/><br/> 
+                <u>  Message</u> : '.  @$_POST["message"]     ;
+
                 if (!$mail->send()) {
-                    echo 'Message bien envoyé';
-                } else{
                     echo $mail->ErrorInfo;
+              } else{
+                    echo 'Message bien envoyé';
               }
             } 
             else {
